315688ed906391ab1525826abf6265c5eed800a8
DaviLimaPereira <davilimapereira@hotmail.com>
2022-08-31 21:08:04 +0000

    Atualizaçoes de navbar

--- src/main/java/com/wintech/sge/Controllers/IndexController.java
+++ src/main/java/com/wintech/sge/Controllers/IndexController.java
@@ -1,11 +1,16 @@
-package com.wintech.sge;
+package com.wintech.sge.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController{
     
     //método para executar a page index
-    @RequestMapping("/")
-    public String index(){
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("titulo", "Home - sge");
         return "index";
     }
 }
